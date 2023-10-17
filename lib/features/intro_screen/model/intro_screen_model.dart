a2c73c21b941b7b6c7b728274fbb95368fd7758a
ubitechparth <paarthbhansali@gmail.com>
2023-10-18 00:04:00 +0530

    intro screen and splash

---
+++ lib/features/intro_screen/model/intro_screen_model.dart
@@ -0,0 +1,3 @@
+class introScreen{
+
+}
--- lib/features/splash_screen/view_model/splash_screen_view_model.dart
+++ lib/features/splash_screen/view_model/splash_screen_view_model.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 class SplashScreeViewModel extends ChangeNotifier {
 
   onInit() {
+
       // Timer(const Duration(seconds: 2), () {
       //   Navigator.of(context as Buil dContext).pushAndRemoveUntil(MaterialPageRoute(builder: (_) =>SliderScreen())
       //   )
