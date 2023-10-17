0f5c6a294ab84e0b1c1a45b87323d051a04deca6
ubitechparth <paarthbhansali@gmail.com>
2023-10-18 00:00:22 +0530

    intro screen and splash

--- lib/features/splash_screen/view_model/splash_screen_view_model.dart
+++ lib/features/splash_screen/view_model/splash_screen_view_model.dart
@@ -1,16 +1,13 @@
-import 'dart:async';
-import 'dart:js';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import '../../../intro_screen.dart';
 
-class SplashScreeViewModel extends ChangeNotifier { 
 
-  onInit(){
-      Timer(const Duration(seconds: 2), () {
-        Navigator.of(context as BuildContext).pushAndRemoveUntil(MaterialPageRoute(builder: (_) =>SliderScreen())
-        )
-      });
+class SplashScreeViewModel extends ChangeNotifier {
 
+  onInit() {
+      // Timer(const Duration(seconds: 2), () {
+      //   Navigator.of(context as Buil dContext).pushAndRemoveUntil(MaterialPageRoute(builder: (_) =>SliderScreen())
+      //   )
+      // });
   }
 }
